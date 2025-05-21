25023b42f8fd5165bfd4532e0ee8d431aef9732c
Rentagawa <miguelangelocabrera1@gmail.com>
2025-05-21 23:45:57 +0800

    Gumana Kana

--- talentscout/talentscout/wsgi.py
+++ talentscout/talentscout/wsgi.py
@@ -1,6 +1,12 @@
 import os
 from django.core.wsgi import get_wsgi_application
+import nltk
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'talentscout.settings')
 
 application = get_wsgi_application()
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
